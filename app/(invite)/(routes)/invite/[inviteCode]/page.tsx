972b47c7a8b08ac1d9d909d988fb51733f10c333
iRE-X <harekrishna8016@gmail.com>
2024-02-26 20:54:22 +0530

    Invite Link Activated

---
+++ app/(invite)/(routes)/invite/[inviteCode]/page.tsx
@@ -0,0 +1,55 @@
+import currentProfile from "@/lib/current-profile";
+import prisma from "@/prisma/db";
+import { redirectToSignIn } from "@clerk/nextjs";
+import { notFound, redirect } from "next/navigation";
+
+interface Props {
+    params: {
+        inviteCode: string;
+    };
+}
+
+const InvitePage = async ({ params: { inviteCode } }: Props) => {
+    const profile = await currentProfile();
+
+    if (!profile) return redirectToSignIn();
+    if (!inviteCode) return redirect("/");
+
+    const valid = await prisma.server.findUnique({
+        where: {
+            inviteCode,
+        },
+    });
+
+    if (!valid) notFound();
+
+    const existingServer = await prisma.server.findUnique({
+        where: {
+            inviteCode,
+            members: {
+                some: {
+                    profileId: profile.id,
+                },
+            },
+        },
+    });
+
+    if (existingServer) return redirect(`/servers/${existingServer?.id}`);
+
+    const server = await prisma.server.update({
+        where: {
+            inviteCode,
+        },
+        data: {
+            members: {
+                create: [{ profileId: profile.id }],
+            },
+        },
+    });
+
+    if (server) return redirect(`/servers/${server.id}`);
+
+    return null;
+};
+
+export default InvitePage;
