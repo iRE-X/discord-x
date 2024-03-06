38a00eaedaa57362de422a38e2a1c2fa8f95153c
iRE-X <harekrishna8016@gmail.com>
2024-03-06 10:16:03 +0530

    Test : Mobile View Menu Button 2

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -2,6 +2,7 @@ import MediaRoom from "@/components/Media-Room";
 import ChatHeader from "@/components/chat/Chat-Header";
 import ChatInput from "@/components/chat/Chat-Input";
 import ChatMessages from "@/components/chat/Chat-Messages";
+import MobileToggle from "@/components/mobile-toggle";
 
 import currentProfile from "@/lib/current-profile";
 import prisma from "@/prisma/db";
@@ -36,41 +37,44 @@ const ChannelPage = async ({ params: { channelId, serverId } }: Props) => {
     if (!channel || !member) return redirect("/");
 
     return (
-        <div className="bg-white dark:bg-[#313338] flex flex-col h-screen">
-            <ChatHeader
-                name={channel.name}
-                serverId={serverId}
-                type="channel"
-            />
-            {channel.type === "TEXT" && (
-                <>
-                    <ChatMessages
-                        name={channel.name}
-                        type="channel"
-                        member={member}
-                        apiUrl="/api/messages"
-                        socketUrl="/api/messages"
-                        socketQuery={{ channelId, serverId }}
-                        paramKey="channelId"
-                        paramValue={channelId}
-                        chatId={channelId}
-                    />
-                    <ChatInput
-                        apiUrl="/api/messages"
-                        type="channel"
-                        name={channel.name}
-                        query={{ channelId, serverId }}
-                    />
-                </>
-            )}
+        <div className="flex items-center">
+            <MobileToggle serverId={serverId} />
+            <div className="bg-white dark:bg-[#313338] flex-1 flex flex-col h-screen">
+                <ChatHeader
+                    name={channel.name}
+                    serverId={serverId}
+                    type="channel"
+                />
+                {channel.type === "TEXT" && (
+                    <>
+                        <ChatMessages
+                            name={channel.name}
+                            type="channel"
+                            member={member}
+                            apiUrl="/api/messages"
+                            socketUrl="/api/messages"
+                            socketQuery={{ channelId, serverId }}
+                            paramKey="channelId"
+                            paramValue={channelId}
+                            chatId={channelId}
+                        />
+                        <ChatInput
+                            apiUrl="/api/messages"
+                            type="channel"
+                            name={channel.name}
+                            query={{ channelId, serverId }}
+                        />
+                    </>
+                )}
 
-            {channel.type === "AUDIO" && (
-                <MediaRoom chatId={channel.id} video={false} audio />
-            )}
+                {channel.type === "AUDIO" && (
+                    <MediaRoom chatId={channel.id} video={false} audio />
+                )}
 
-            {channel.type === "VIDEO" && (
-                <MediaRoom chatId={channel.id} video audio />
-            )}
+                {channel.type === "VIDEO" && (
+                    <MediaRoom chatId={channel.id} video audio />
+                )}
+            </div>
         </div>
     );
 };
--- components/mobile-toggle.tsx
+++ components/mobile-toggle.tsx
@@ -1,14 +1,14 @@
 import NavigationSidebar from "@/components/navigation/Navigation-Sidebar";
 import ServerSidebar from "@/components/server/Server-Sidebar";
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
-import { Menu } from "lucide-react";
+import { ChevronRight, Menu } from "lucide-react";
 
 const MobileToggle = ({ serverId }: { serverId: string }) => {
     return (
         <Sheet>
             <SheetTrigger asChild>
-                <button className="fixed md:hidden mr-2">
-                    <Menu />
+                <button className="md:hidden mr-2">
+                    <ChevronRight className="absolute w-3 h-[25vh] bg-zinc-200 dark:bg-zinc-600 rounded-r-full" />
                 </button>
             </SheetTrigger>
             <SheetContent side="left" className="p-0 flex gap-0">
