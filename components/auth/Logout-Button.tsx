0467a2d913009a97dc1798e8cfecbc4f95b0cc71
iRE-X <harekrishna8016@gmail.com>
2024-05-29 20:44:05 +0530

    Fix - CallbackUrl after Logout

--- components/auth/Logout-Button.tsx
+++ components/auth/Logout-Button.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { signOut } from "next-auth/react";
 import React from "react";
+import { FORGOT_PASSWORD_URL, LOGIN_URL } from "@/routes";
 
 interface Props {
   children?: React.ReactNode;
@@ -9,7 +10,7 @@ interface Props {
 const LogoutButton = ({ children }: Props) => {
   return (
     <span
-      onClick={() => signOut({ callbackUrl: "/" })}
+      onClick={() => signOut({ callbackUrl: LOGIN_URL })}
       className="cursor-pointer flex items-center justify-center p-2"
     >
       {children}
