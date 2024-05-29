d1d62c96b9f695f7374f8aef30cbe6a025467529
iRE-X <harekrishna8016@gmail.com>
2024-05-29 21:10:44 +0530

    Fix - Redirect To Login page after Logout

--- components/auth/Logout-Button.tsx
+++ components/auth/Logout-Button.tsx
@@ -2,15 +2,24 @@
 import { signOut } from "next-auth/react";
 import React from "react";
 import { LOGIN_URL } from "@/routes";
+import useOrigin from "@/hooks/useOrigin";
 
 interface Props {
   children?: React.ReactNode;
 }
 
 const LogoutButton = ({ children }: Props) => {
+  const origin = useOrigin();
+  const redirectUrl = new URL(LOGIN_URL, origin).toString();
+
   return (
     <span
-      onClick={() => signOut({ callbackUrl: LOGIN_URL, redirect: true })}
+      onClick={() =>
+        signOut({
+          callbackUrl: redirectUrl,
+          redirect: true,
+        })
+      }
       className="cursor-pointer flex items-center justify-center p-2"
     >
       {children}
